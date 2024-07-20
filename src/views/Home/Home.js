279f143773b76c53a60c389c7afb4bb080e781da
sakshishewale <sakshishewale2808@gmail.com>
2024-07-20 17:56:28 +0530

    correcting the code

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,57 +1,56 @@
-import React, { useEffect, useState } from 'react';
-import "./Home.css";
-import PlantCard from "./../../components/PlantCard/PlantCard";
-import axios from 'axios';
-import toast, { Toaster } from "react-hot-toast";
-
+import React, { useEffect, useState } from 'react'
+import "./Home.css"
+import PlantCard from '../../components/PlantCard/PlantCard'
+import axios from 'axios'
+import toast, { Toaster} from "react-hot-toast"
+import imgAdd from "./add.png"
+import { Link } from 'react-router-dom'
 function Home() {
-    const [plants, setPlants] = useState([]);
+  
+   const [Plants ,setPlants]=useState([])
 
-    const loadPlants = async () => {
-        try {
-            toast.loading("Loading plants...");
-            const response = await axios.get('http://localhost:8000/plants');
-            setPlants(response.data.data);
-            toast.dismiss();
-            toast.success("Plants loaded successfully");
-        } catch (error) {
-            toast.dismiss();
-            toast.error("Failed to load plants");
-            console.error("Error loading plants:", error);
-        }
-    };
-
-    useEffect(() => {
-        loadPlants();
-    }, []); 
-
-    return (
-        <div>
-            <h1>Plants</h1>
-            <div className="plant-container">
-                {plants.map((plant, i) => {
-                    const {
-                        name,
-                        category,
-                        image,
-                        price,
-                        description
-                    } = plant;
-                    return (
-                        <PlantCard
-                            key={i}
-                            name={name}
-                            category={category}
-                            image={image}
-                            price={price}
-                            description={description}
-                        />
-                    );
-                })}
-            </div>
-            <Toaster />
-        </div>
-    );
+   const loadPlants= async ()=>{
+   toast.loading("Loading Plants...")
+  const response  = await axios.get(`http://localhost:8000/Plants`)
+  toast.dismiss()
+  toast.success("Plant loaded successfully!")
+  setPlants(response.data.data)
+   }
+   useEffect(()=>{
+    loadPlants()
+   },[])
+  return (
+    <div className='plants-container'>
+    
+      <h1>Plants</h1>
+      {
+        Plants.map((Plant,i)=>{
+            const {
+              _id,
+                name,
+                category,
+                price,
+                description
+            }=Plant
+            return( <PlantCard 
+                key={i}
+                _id={_id} 
+                name={name}
+                 category={category}
+                  price={price}
+                   description={description}
+                 
+                   loadPlants={ loadPlants}  />
+                  )
+        
+                  })
+      }
+    <Toaster  />
+    <Link to="/add">
+    <img src={imgAdd}className='btn-add'/>
+    </Link>
+    </div>
+  )
 }
 
-export default Home;
+export default Home
